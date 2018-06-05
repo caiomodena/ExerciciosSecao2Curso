f75f152e444d54e4c9d65794ae53a23cf2f6c40d
caiomodena <caio.modena@gmail.com>
2018-06-05 20:58:33 -0300

    sobrecarga e atributos privados

--- RH/Funcionario.cs
+++ RH/Funcionario.cs
@@ -6,13 +6,19 @@ namespace RH
     class Funcionario{
 
         public String nome;
-        public double salarioBruto;
-        public double desconto;
+        private double salarioBruto {get; set;}
+        private double desconto {get; set;}
 
         public Funcionario(String nome, double salarioBruto, double desconto){
             this.nome = nome;
             this.salarioBruto = salarioBruto;
             this.desconto = desconto;
+        }
+
+        public Funcionario(String nome){
+            this.nome = nome;
+            this.salarioBruto = 0;
+            this.desconto = 0;
         }
 
         public double salarioLiquido(){
