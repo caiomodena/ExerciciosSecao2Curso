f3ed4584a0aff30138a13428e011dbdeca9551ef
caiomodena <caio.modena@gmail.com>
2018-06-11 22:18:14 -0300

    finalizando projeto Banco

--- Banco/Banco.cs
+++ Banco/Banco.cs
@@ -5,20 +5,20 @@ namespace Banco
 {
     class Banco{
 
-        int numeroConta;
-        String nomeTitularConta {set;}
-        double valorDepositoInicial;
-        double saldo;
+        public int numeroConta {get; private set;}
+        public string nomeTitularConta {get;set;}
+        public double valorDepositoInicial {get; set;}
+        public double saldo {get; private set;}
 
-        public Banco(int numeroConta, String nomeTitularConta, double valorDepositoInicial){
+        public Banco(int numeroConta, string nomeTitularConta, double valorDepositoInicial){
             this.numeroConta = numeroConta;
             this.nomeTitularConta = nomeTitularConta;
-            this.valorDepositoInicial = valorDepositoInicial;
+            saldo = valorDepositoInicial;
         }
-        public Banco(int numeroConta, String nomeTitularConta){
+        public Banco(int numeroConta, string nomeTitularConta){
             this.numeroConta = numeroConta;
             this.nomeTitularConta = nomeTitularConta;
-            valorDepositoInicial = 0;
+            saldo = 0;
         }
 
         public void depositar(double valor){
--- Banco/Program.cs
+++ Banco/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Banco
 {
@@ -6,7 +7,35 @@ namespace Banco
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Digite o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do titular da conta: ");
+            string nomeTitular =  Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial <s/n>? ");
+            char escolha = char.Parse(Console.ReadLine());
+
+            if(escolha=='s'){
+                Console.Write("Digite o valor do depósito inicial: ");
+                double valorDeposito = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                Banco banco1 = new Banco(numeroConta,nomeTitular,valorDeposito);
+
+                Console.WriteLine("Conta criada: ");
+                Console.WriteLine("Conta {0}, Titular: {1}, Saldo: R$ {2}",banco1.numeroConta,
+                    banco1.nomeTitularConta,banco1.saldo);
+
+                Console.Write("Digite um valor para depósito: ");
+                double valorDeposito2 = Double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                banco1.depositar(valorDeposito2);
+
+                Console.WriteLine("Conta {0}, Titular: {1}, Saldo: R$ {2}",banco1.numeroConta,
+                    banco1.nomeTitularConta,banco1.saldo);
+            }
+
+            
+
         }
     }
 }
