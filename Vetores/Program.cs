4b95274f820b9d389297a227c09af8cff51f6141
caiomodena <caio.modena@gmail.com>
2018-06-26 21:39:55 -0300

    Exercicios Vetores

---
+++ Vetores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vet;
+            double soma=0;
+            double media=0;
+
+            int num = int.Parse(Console.ReadLine());
+
+            vet = new double[num];
+
+            for(int i = 0;i<num;i++){
+                vet[i] = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            }
+
+            for(int i=0;i<num;i++){
+                soma+=vet[i];
+            }
+            
+            media=soma/num;
+
+            Console.WriteLine("Altura média: {0}",media.ToString("F2"));
+        }
+    }
+}
