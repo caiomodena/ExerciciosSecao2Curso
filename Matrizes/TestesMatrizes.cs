291e1ecf794c1e5aa8be0ee47fbf6621c7d0dc26
caiomodena <caio.modena@gmail.com>
2018-09-02 19:03:31 -0300

    Matrizes

---
+++ Matrizes/TestesMatrizes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz;
+            int numero;
+
+            numero = int.Parse(Console.ReadLine()); 
+            
+            matriz = new int[numero,numero];
+
+            for(int i=0;i<numero;i++){
+                string[] linhaDigitada = Console.ReadLine().Split(' ');
+                for(int j=0;j<numero;j++){
+                    matriz[i,j] = int.Parse(linhaDigitada[j]);
+                }
+            }
+
+            Console.WriteLine("Diagonal Principal: ");
+            for(int i=0;i<numero;i++){                
+                Console.Write(matriz[i,i]+" ");
+            }
+            Console.WriteLine();
+
+            int contadorNegativos=0;
+            
+            for(int j=0;j<numero;j++){
+                for(int a=0;a<numero;a++){
+                    if(matriz[j,a]<0){
+                        contadorNegativos++;
+                    }
+                }
+            }
+            Console.WriteLine("Quantidade de negativos: ");
+            Console.Write(contadorNegativos);
+        }
+    }
+}
