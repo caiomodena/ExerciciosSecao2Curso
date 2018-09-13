bf110d5c944cf1e6dae7231b090658e53133c248
caiomodena <caio.modena@gmail.com>
2018-09-13 19:03:23 -0300

    exercício 1 de listas

---
+++ Listas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic; 
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercício 1 
+            /*List<string> lista = new List<string>(); 
+            
+            lista.Add("Caio");
+            lista.Add("João");
+            lista.Add("Ana");
+
+            lista.Remove("João");
+            lista.RemoveAt(0);
+
+            int pos = lista.FindIndex(x => x == "Ana");
+
+            Console.WriteLine("posição da Ana é: "+pos);
+
+            for(int i=0;i<lista.Count;i++){
+                Console.WriteLine(lista[i]);
+            }
+            */
+            //Fim Exercício 1
+        }
+    }
+}
