9f30b2419a3a7377c30c230d91405455a84b1b03
caiomodena <caio.modena@gmail.com>
2018-09-30 09:37:02 -0300

    Elementos Estáticos

---
+++ ElementosEstaticos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ElementosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--Digite três números inteiros--");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int menorNumeroDigitado = Util.menorEntreTres(num1,num2,num3);
+            Console.WriteLine("O menor número digitado é o: "+menorNumeroDigitado);
+        }
+    }
+}
