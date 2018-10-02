28cdda62e1f948e59da4e109b8f720f150e16528
caiomodena <caiom@bempromotora.com.br>
2018-10-02 13:19:45 -0300

    finalizando exercício de produtos e pedidos

--- ExercicioProdutosPedidos/Program.cs
+++ ExercicioProdutosPedidos/Program.cs
@@ -7,16 +7,18 @@ namespace ExercicioProdutosPedidos
         static void Main(string[] args)
         {
             Estoque estoque = new Estoque();
-            ItemPedido itemPedido = new ItemPedido();            
+            ItemPedido itemPedido = new ItemPedido();  
+            Pedido pedido = new Pedido();
             int opcao = 1;
-            Console.WriteLine("--Menu de Produtos e Pedidos--");
-            Console.WriteLine("1 - Listar produtos ordenadamente");
-            Console.WriteLine("2 - Cadastrar produto");
-            Console.WriteLine("3 - Cadastrar pedido");
-            Console.WriteLine("4 - Mostrar dados de um pedido");
-            Console.WriteLine("5 - Sair");
-            Console.Write("Digite a opção desejada: ");
             while(opcao!=5){
+                Console.WriteLine("--Menu de Produtos e Pedidos--");
+                Console.WriteLine("1 - Listar produtos ordenadamente");
+                Console.WriteLine("2 - Cadastrar produto");
+                Console.WriteLine("3 - Cadastrar pedido");
+                Console.WriteLine("4 - Mostrar dados de um pedido");
+                Console.WriteLine("5 - Sair");
+                Console.Write("Digite a opção desejada: ");
+            
                 opcao = int.Parse(Console.ReadLine());
             
                 switch(opcao){
@@ -25,6 +27,8 @@ namespace ExercicioProdutosPedidos
                     case 2: estoque.cadastrarProdutoTela();
                             break;
                     case 3: itemPedido.cadastrarPedidoTela();
+                            break;
+                    case 4: pedido.selecionaDadosPedido();
                             break;
                     default: Console.WriteLine("Fim!");
                             break;
